f924305b2b50d6f1e03c02dc0d13d4f84391cf74
Dominic Grenier <themoustafa@hotmail.com>
2022-02-09 15:23:37 -0500

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,22 @@
 import Render from "./render"
 import Assets from "./assets"
 import * as NeW from "./utils/cells/NwParser"
-import cellTile from "./utils/cells/type"
+import {cellTile} from "./utils/cells/type"
 
-import {promises as fs} from 'node:fs'
-import * as path from 'path'
-
-var gameWidth = 800;
-var gameHeight = 600;
+import Globals from "./globals"
 
 const init = async () => {
   
   let assetsToLoad: {[id:string]: string} = {};
-  assetsToLoad['tileset'] = 'http://localhost:8003/assets/pics2.png'
-  assetsToLoad['cellFile'] = 'http://localhost:8003/nwfiles/zo_ay25.nw'
-  let assets = new Assets();
+  assetsToLoad['tileset'] = Globals.getOrigin()+'/assets/pics2.png'
+  assetsToLoad['cellFile'] = Globals.getOrigin()+'/assets/nwfiles/contestfoyer.nw'
 
   await Assets.loadAssets(assetsToLoad)
-  // console.log(Assets.getText('tileset'))
-  // .then(()=>{
   const cell : Array<cellTile> = NeW.default.parseCell(Assets.getText('cellFile'))
-  console.log(cell)
-  console.log(Assets.getImage('tileset'))
 
   Render.main()
-
-  Render.renderASquare(cell)
+  Render.renderALevel(cell, 16, 64)
     
-  //     }
-  //   )
-    
-    
-    // const cellData : string = await fs.readFile(, 'utf8')
-  }
+}
   
   init()
-  // Render.main();
