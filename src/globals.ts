c41b849de5c7a95df7392a702564f0b18c544e01
Dominic Grenier <themoustafa@hotmail.com>
2022-02-09 15:23:05 -0500

    Created a global class/namespace

---
+++ src/globals.ts
@@ -0,0 +1,30 @@
+
+export default class Globals {
+  private static gameWidth : number = 1024;
+  private static gameHeight : number = 1024;
+
+  private static origin : string = 'http://localhost:8003'
+
+  private static imgExts: Array<string> = ['png','jpg']
+  private static txtExts: Array<string> = ['nw','txt']
+
+  public static getGameWidth() : number {
+    return Globals.gameWidth
+  }
+  public static getGameHeight() : number {
+    return Globals.gameHeight
+  }
+
+  public static getOrigin() : string {
+    return Globals.origin
+  }
+
+  public static getTxtExts() : Array<string> {
+    return Globals.txtExts;
+  }
+
+  public static getImgExts() : Array<string> {
+    return Globals.imgExts;
+  }
+
+}
