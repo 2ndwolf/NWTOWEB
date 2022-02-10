0fdab0cbd3244c965c2e9a3e061d277bc3b7ab41
Dominic Grenier <themoustafa@hotmail.com>
2022-02-09 22:38:01 -0500

    Extended the renderable type

--- src/utils/cells/type.ts
+++ src/utils/cells/type.ts
@@ -11,5 +11,42 @@ export enum LevelTypes {
   whatever,
 }
 
+export enum shaderPropTypes {
+  attribute,
+  uniform
+}
 
+export type shaderProp = {
+  // varName: string,
+  propType: shaderPropTypes,
+  data: Float32Array,
+}
+
+export type shaderProperties = {
+  properties: {[propName: string]: shaderProp}
+  program: WebGLProgram
+}
+
+export type renderableWProps = {
+  id: string,
+  x: number,
+  y: number,
+  scale: Float32Array,
+  angle: number,
+  texture: WebGLTexture
+}
+
+export type shaderPass = {
+  fnct : (self: renderableBatch) => void,
+}
+
+export type renderableBatch = {
+  r: {[layer:number]: Array<renderableWProps>},
+  shader: shaderProperties,
+  passes: {[num:number]: shaderPass}
+}
+
+export type renderables = {
+  all: {[layer:number]: Array<renderableBatch>}
+}
 // export default cellTile
